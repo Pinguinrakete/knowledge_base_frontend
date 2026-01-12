a401a1c832f11eedbee5f80db90c0047e451125e
Pinguinrakete <ekirschke@web.de>
2026-01-12 15:09:40 +0100

    Add to navbar add and back

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function openAddArticle() {
     document.getElementById('agb_section').classList.add('d_none');
     document.getElementById('impressum_section').classList.add('d_none');
     document.getElementById('add_section').classList.remove('d_none');
+    document.getElementById('link_add_article').classList.add('d_none');
+    document.getElementById('link_back').classList.remove('d_none');
 }
 
 function openAGB() {
@@ -14,6 +16,8 @@ function openAGB() {
     document.getElementById('agb_section').classList.remove('d_none');
     document.getElementById('impressum_section').classList.add('d_none')
     document.getElementById('add_section').classList.add('d_none');
+    document.getElementById('link_back').classList.add('d_none');
+    document.getElementById('link_add_article').classList.remove('d_none');
 }
 
 function openImpressum() {
@@ -23,7 +27,18 @@ function openImpressum() {
     document.getElementById('agb_section').classList.add('d_none');
     document.getElementById('impressum_section').classList.remove('d_none');
     document.getElementById('add_section').classList.add('d_none');
+    document.getElementById('link_back').classList.add('d_none');
+    document.getElementById('link_add_article').classList.remove('d_none');
 }
 
-
+function openMainMenu() {
+    document.getElementById('search_container').classList.add('d_fd_c_ai_c');
+    document.getElementById('btn_hinzufuegen').classList.remove('d_none');
+    document.getElementById('position_article').classList.remove('d_none');
+    document.getElementById('agb_section').classList.add('d_none');
+    document.getElementById('impressum_section').classList.add('d_none');
+    document.getElementById('add_section').classList.add('d_none');
+    document.getElementById('link_add_article').classList.remove('d_none');
+    document.getElementById('link_back').classList.add('d_none');
+}
 
